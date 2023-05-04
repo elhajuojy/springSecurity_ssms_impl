e216d2230010c6e7c3af1b1ed345f27eb018a021
elhajuojy <souirimehdi311@gmail.com>
2023-05-04 14:35:47 +0100

    add basic endpoint and dependency

--- src/main/java/com/elhjuojy/springsecurity_ssms_impl/SpringSecuritySsmsImplApplication.java
+++ src/main/java/com/elhjuojy/springsecurity_ssms_impl/SpringSecuritySsmsImplApplication.java
@@ -2,6 +2,9 @@ package com.elhjuojy.springsecurity_ssms_impl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecuritySsmsImplApplication {
@@ -11,4 +14,15 @@ public class SpringSecuritySsmsImplApplication {
         SpringApplication.run(SpringSecuritySsmsImplApplication.class, args);
     }
 
+
+
+
+}
+
+@RestController
+class BasicController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> get(){
+        return ResponseEntity.ok("Hello");
+    }
 }
