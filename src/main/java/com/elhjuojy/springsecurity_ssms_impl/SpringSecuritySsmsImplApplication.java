9bf32fe4506e39eeda60e5c888b551f92e22fe86
elhajuojy <souirimehdi311@gmail.com>
2023-05-04 11:47:51 +0100

    Initial commit

---
+++ src/main/java/com/elhjuojy/springsecurity_ssms_impl/SpringSecuritySsmsImplApplication.java
@@ -0,0 +1,13 @@
+package com.elhjuojy.springsecurity_ssms_impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySsmsImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySsmsImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elhjuojy/springsecurity_ssms_impl/SpringSecuritySsmsImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elhjuojy.springsecurity_ssms_impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySsmsImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
